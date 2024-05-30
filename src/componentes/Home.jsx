a9a9226aae9f92d84a4f31fd00e74c5d884340ae
Jesus Soto <jesusescar123@gmail.com>
2024-05-29 22:35:33 -0400

    primer componente

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Home.css";
+import InformacionUsuario from "./InformacionUsuario";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <header className="header">
+        <h1 className="no-margin no-padding">Desarrollo de los componentes</h1>
+      </header>
+      {/* FIN HEADER */}
+      <div className="contenedor">
+        <div className="contenido-principal">
+          <p className="no-margin">Primer componente</p>
+          <div className="info-usuario">
+            <InformacionUsuario />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
