a537737c9799d85a3fbcd61413ab4d982703a75a
Aldahir Fernandez <aldahirfernandez27@gmail.com>
2024-06-06 09:44:05 -0400

    Estilo mejorado

--- src/componentes/DescripEntrenador.jsx
+++ src/componentes/DescripEntrenador.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./DescripEntrenador.css"
-import Entrenador2 from '../assets/Entrenador2.png';
+import Entrenador21 from '../assets/Entrenador21.png';
 
 const DescripEntrenador = () => {
 
   return (
     <div className='descrip-container'>
-        <img src={Entrenador2} alt="" />
-        <div>
+        <img src={Entrenador21} alt="" />
+        <div className='text-container'>
             <h3>Alex Pheonix</h3>
             <p>alexpheonix1@gmail.com</p>
         </div>
