f28839e44f8b09f3a849260b1da3511920e86f31
Victor Alvarez <bolivarisimo1404@gmail.com>
2024-06-12 22:38:53 -0400

    17

--- src/navegacion/Navegacion.jsx
+++ src/navegacion/Navegacion.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../componentes/BodyPresentacion/Presentacion";
+import PersonalTrainerProfile from "../componentes/PerfEntrenador/PerfEntrenador";
 
 const Navegacion = () => {
   return (
@@ -8,6 +9,7 @@ const Navegacion = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/PerfEntrenador" element={<PersonalTrainerProfile />} />
         </Routes>
       </Router>
     </>
